d39fc845da092f745b6b8ea2a38bb58c6693c7ca
Gustavo Chen <gustavo.sao@gmail.com>
2023-10-18 21:28:56 -0300

    Remove bad styled components package

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -251,11 +251,6 @@ _G.packer_plugins = {
     path = "/home/guschen/.local/share/nvim/site/pack/packer/start/vim-commentary",
     url = "https://github.com/tpope/vim-commentary"
   },
-  ["vim-styled-components"] = {
-    loaded = true,
-    path = "/home/guschen/.local/share/nvim/site/pack/packer/start/vim-styled-components",
-    url = "https://github.com/styled-components/vim-styled-components"
-  },
   ["vim-visual-multi"] = {
     loaded = true,
     path = "/home/guschen/.local/share/nvim/site/pack/packer/start/vim-visual-multi",
