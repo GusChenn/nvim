66ab76dbe57e554316bb46d880e16b99c7e20b62
Gustavo Chen <gustavo.sao@gmail.com>
2023-12-19 16:07:11 -0300

    Make harpoon only show the file name

--- after/plugin/coding-related/harpoon.lua
+++ after/plugin/coding-related/harpoon.lua
@@ -5,19 +5,38 @@ if not harpoon then
 	return
 end
 
-harpoon:setup({})
+local format_list_item = function(list_item)
+	-- return only the part after the last / character on the path
+	return list_item.value:match("^.+/(.+)$")
+end
+
+harpoon:setup({
+	settings = {
+		save_on_toggle = true,
+	},
+	default = {
+		display = format_list_item,
+	},
+})
 
 -- Set keymaps
 
 vim.keymap.set("n", "<leader>pp", function()
 	harpoon:list():append()
 end)
+
+-- to open quick menu
 vim.keymap.set("n", "<leader>p", function()
 	harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
+
+-- To open telescope preview
+-- vim.api.nvim_set_keymap("n", "<leader>p", ":Telescope harpoon marks<CR>", { noremap = true, silent = true })
+--
 vim.keymap.set("n", "<leader>l", function()
 	harpoon:list():next()
 end)
+
 vim.keymap.set("n", "<leader>h", function()
 	harpoon:list():prev()
 end)
