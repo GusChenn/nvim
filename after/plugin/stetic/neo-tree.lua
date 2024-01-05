415ed4dbee1b1398259fe147796a10b4df1e3224
Gustavo Chen <gustavo.sao@gmail.com>
2024-01-05 18:19:34 -0300

    Add pane switch while in terminal

--- after/plugin/stetic/neo-tree.lua
+++ after/plugin/stetic/neo-tree.lua
@@ -16,4 +16,18 @@ neotree.setup({
 			nowait = true,
 		},
 	},
+	filesystem = {
+		filtered_items = {
+			visible = true,
+			show_hidden_count = true,
+			hide_dotfiles = false,
+			hide_gitignored = true,
+			hide_by_name = {
+				-- '.git',
+				-- '.DS_Store',
+				-- 'thumbs.db',
+			},
+			never_show = {},
+		},
+	},
 })
