9a59f1ab266bf7c42312c6f3cec8eeef0ba3eceb
Gustavo Chen <gustavo.sao@gmail.com>
2024-12-11 17:59:14 -0300

    Removed long configs folder

--- lua/plugins/interface.lua
+++ lua/plugins/interface.lua
@@ -118,7 +118,36 @@ return {
   {
     "shellRaining/hlchunk.nvim",
     event = { "UIEnter" },
-    opts = require "config.long-configs.hlchunk",
+    opts = {
+      chunk = {
+        enable = true,
+        use_treesitter = true,
+        chars = {
+          horizontal_line = "─",
+          left_top = "┌",
+          vertical_line = "│",
+          left_bottom = "└",
+          right_arrow = "─",
+        },
+        style = {
+          {
+            -- TODO: Get color from theme
+            fg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID "MatchParen"), "bg", "gui"),
+          },
+        },
+        max_file_size = 80 * 1024,
+        delay = 0,
+      },
+      indent = {
+        enable = false,
+      },
+      line_num = {
+        enable = false,
+      },
+      blank = {
+        enable = false,
+      },
+    },
   },
   {
     "RRethy/vim-illuminate",
@@ -297,7 +326,14 @@ return {
   },
   {
     "windwp/nvim-ts-autotag",
-    ft = { "typescriptreact", "tsx", "html", "eruby" },
+    ft = {
+      "typescriptreact",
+      "tsx",
+      "html",
+      "xml",
+      "eruby",
+      "embedded_template",
+    },
     config = true,
   },
   {
