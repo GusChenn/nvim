fcd9b1130d992f20039d3764033addca11c1c5d0
Gustavo Chen <gustavo.sao@gmail.com>
2025-05-27 19:18:46 -0300

    Add codecompanion history

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -176,7 +176,8 @@ return {
       "nvim-telescope/telescope.nvim",
       "Davidyz/VectorCode",
       "ravitemer/mcphub.nvim",
-      "banjo/contextfiles.nvim"
+      "banjo/contextfiles.nvim",
+      "ravitemer/codecompanion-history.nvim"
     },
     event = "VeryLazy",
     opts = function()
@@ -336,6 +337,37 @@ return {
               -- your contextfiles configuration here
               -- or leave it empty to use the default configuration
             },
+          },
+          history = {
+            enabled = true,
+            opts = {
+              -- Keymap to open history from chat buffer (default: gh)
+              keymap = "gh",
+              -- Keymap to save the current chat manually (when auto_save is disabled)
+              save_chat_keymap = "sc",
+              -- Save all chats by default (disable to save only manually using 'sc')
+              auto_save = true,
+              -- Number of days after which chats are automatically deleted (0 to disable)
+              expiration_days = 10,
+              -- Picker interface (auto resolved to a valid picker)
+              picker = "telescope", --- ("telescope", "snacks", "fzf-lua", or "default") 
+              ---Automatically generate titles for new chats
+              auto_generate_title = true,
+              title_generation_opts = {
+                ---Adapter for generating titles (defaults to current chat adapter) 
+                adapter = nil, -- "copilot"
+                ---Model for generating titles (defaults to current chat model)
+                model = nil, -- "gpt-4o"
+              },
+              ---On exiting and entering neovim, loads the last chat on opening chat
+              continue_last_chat = true,
+              ---When chat is cleared with `gx` delete the chat from history
+              delete_on_clearing_chat = false,
+              ---Directory path to save the chats
+              dir_to_save = vim.fn.stdpath("data") .. "/codecompanion-history",
+              ---Enable detailed logging for history extension
+              enable_logging = false,
+            }
           },
         }
       }
