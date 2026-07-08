f9f96670a325f0218e3ef9e4db97a45e5af72e6c
Gustavo Chen <gustavo.sao@gmail.com>
2026-07-08 12:42:59 -0300

    Fix neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -3,21 +3,12 @@ vim.pack.add({ "https://github.com/neogitorg/neogit" })
 vim.keymap.set("n", "gs", "<cmd>Neogit<CR>", { desc = "Git status" })
 
 require("neogit").setup({
-  auto_show_console = true,
   disable_hint = true,
   disable_insert_on_commit = true,
-  filewatcher = {
-    interval = 1000,
-    enabled = true,
-  },
   signs = {
     hunk = { "", "" },
     item = { "➡", "⬇" },
     section = { "➡", "⬇" },
-  },
-  integrations = {
-    fzf_lua = false,
-    mini_pick = true
   },
   mappings = {
     popup = {
