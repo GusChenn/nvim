98aec16560579ffe93c18b895fb09ab42eaa97e4
Gustavo Chen <gustavo.sao@gmail.com>
2026-07-17 20:40:41 -0300

    Allow fg to grep selected text

--- lua/core/picker.lua
+++ lua/core/picker.lua
@@ -8,10 +8,22 @@ vim.pack.add({
 require("mini.pick").setup()
 
 -- vim.keymap.set("n", "ff", MiniPick.builtin.files, { desc = "Find files" })
--- vim.keymap.set("n", "fg", MiniPick.builtin.grep_live, { desc = "Live grep" })
-vim.keymap.set('n', 'fg', function()
-  MiniPick.builtin.grep_live({}, { pattern = 'hello' })
-end, { desc = 'Search word' })
+vim.keymap.set('n', 'fg', MiniPick.builtin.grep_live, { desc = 'Live grep' })
+vim.keymap.set('v', 'fg', function()
+  local save, save_type = vim.fn.getreg('z'), vim.fn.getregtype('z')
+
+  vim.cmd('noautocmd normal! "zy')
+  local chars = vim.fn.split(vim.fn.getreg('z'), '\\zs')
+
+  vim.fn.setreg('z', save, save_type)
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'MiniPickStart',
+    once = true,
+    callback = function() MiniPick.set_picker_query(chars) end,
+  })
+  MiniPick.builtin.grep_live()
+end, { desc = 'Live grep selection' })
+
 vim.keymap.set("n", "fb", MiniPick.builtin.buffers, { desc = "List open buffers" })
 
 
